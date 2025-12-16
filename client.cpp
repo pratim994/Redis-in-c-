47ded1235b52d50ebb3992751977c199f6eb12aa
pratim994 <aryanpratim86@gmail.com>
2025-12-16 14:33:59 +0530

    client module

---
+++ client.cpp
@@ -0,0 +1,45 @@
+#include<stdint.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
+#include<unistd.h>
+#include<arpan/inet.h>
+#include<sys/socket.h>
+#include<netinet/ip.h>
+
+
+static void die(const char *msg){
+    int err = errno;
+    fprintf(stderr,"[%d]%s/n", eer, msg);
+    abort();
+}
+
+
+
+int main(){
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd<0){
+        die("socket()");
+    }
+    struct sockaddr_in addr ={};
+    addr.sin._family = AF_INET;
+    addr.sin._port = noths(1234);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
+    int rv = connect(fd,( const struct sockaddr*)&addr, sizeof(addr));
+    if(rv){
+        die("connect");
+
+    }
+    char msg[] = "hello";
+    write(fd, msg, strlen(msg));
+
+    char rbuf[64] = {};
+    ssize_t n = read(fd, rbuf,sizeof(rbuf)-1);
+    if(n<0){
+        die("read");
+    }
+    printf("server says :[%s]\n",rbuf);
+    close(fd);
+    return 0;
+}
