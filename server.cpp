78d2df13e60bf3b020d782ffc87698d2d4f8a42d
pratim994 <aryanpratim86@gmail.com>
2025-12-17 19:38:32 +0530

    added request parser

--- server.cpp
+++ server.cpp
@@ -116,3 +116,56 @@ static struct{
     g_data.fd2conn[conn->fd] = conn;
     return 0;
  }
+
+ void conn_destroy(conn *conn){
+    (void)close(conn->fd);
+    g_data.fd2conn[conn->fd] = NULL;
+    dlist_detach(&conn->idle_node);
+    delete conn;
+ }
+
+ const size_t k_max_args = 200*1000;
+
+ static bool read_u32(const uint8_t *&cur,const uint8_t *&end, const uint32_t &out){
+    if(cur+4 >end){
+        return false;
+
+    }
+    memcpy(&out, cur, 4);
+    cur += 4;
+    return true;
+ }
+  
+ static bool read_str(const uint8_t *&cur, const uint8_t *&end,size_t n, const uint32_t &out){
+    if(cur+n>end){
+        return false;
+    }
+    out.assign(cur, cur+n);
+    cur+=n;
+    return true;
+ }
+
+ static int32_t parse_req(const uint8_t *data, size_t data, std::vector<std::string> &out){
+    const uint8_t *end = data + size;
+    uint32_t nstr = 0;
+    if(!read_u32(data, end, nstr)){
+        return -1;
+    }
+    if(nstr > k_max_args){
+        return -1;
+    }
+    while(out.size()<nstr){
+        uint32_t len = 0;
+        if(!read_u32(data, end, len)){
+            return -1;
+        }
+        out.push_back(std::string());
+        if(!read_str(data, end, len, out.back())){
+            return -1;
+        }
+    }
+    if(data!= end){
+        return -1;
+    }
+    return 0;
+ }
